1da55d28b6f8d7d3f7e8bc45a395fdb5c6725d38
shashank <shashank1049.be23@chitkara.edu.in>
2025-05-09 13:21:52 +0530

    initial commit

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(cookieParser()); // Use cookie parser to read cookies
+app.use(express.static(path.join(__dirname, 'public'))); // Serve static files from the public directory
 
 // Set EJS as the view engine
 app.set("view engine", "ejs");
